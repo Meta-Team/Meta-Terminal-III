c6bf495d5e2c54734b6a4e17b98084020fefd563
ZhuKerui <kerui.17@intl.zju.edu.cn>
2021-02-11 18:46:57 +0800

    simple socket works

--- virtual_device/socket_server.py
+++ virtual_device/socket_server.py
@@ -1,21 +1,21 @@
-import socket
+import socketserver
 from time import sleep
 
 meta_port = 2021
 
-if __name__ == '__main__':
-    soc_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc_server.bind(('localhost', meta_port))
-    soc_server.listen(1)
-    try:
-        client, addr = soc_server.accept()
+class MyHandler(socketserver.BaseRequestHandler):
+    def handle(self):
         while True:
-            data = client.recv(100)
-            print(data.decode())
-            client.send('hello'.encode('utf-8'))
-            sleep(1)
-    except Exception as e:
-        print(e)
-        print('exception')
-    finally:
-        soc_server.close()
+            try:
+                data = self.request.recv(100)
+                print(data.decode().strip())
+                self.request.send('hello\n'.encode('utf-8'))
+                sleep(1)
+            except:
+                break
+        self.request.close()
+
+
+if __name__ == '__main__':
+    socket_server = socketserver.ThreadingTCPServer(('localhost', meta_port), MyHandler)
+    socket_server.serve_forever()
